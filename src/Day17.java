b9729123b131fcc97ae141ca486d1618b1541010
Johan de Jong <johan.de.jong@sogeti.com>
2022-12-17 12:22:11 +0100

    Day 17 part 2 performance improvements.

--- src/Day17.java
+++ src/Day17.java
@@ -1,13 +1,15 @@
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 
 public class Day17 {
     String moves;
     int moveCount;
-    List<List<Position>> shapes = new ArrayList<>();
+    List<Shape> shapes = new ArrayList<>();
     int shapeCount;
 
     public static void main(String[] args) throws IOException {
@@ -17,7 +19,9 @@ public class Day17 {
     private void start() throws IOException {
         init();
         drop(2022);
-        drop(1000000);
+        long start = new Date().getTime();
+        drop(100000000);
+        System.out.println("ms: " + (new Date().getTime() - start));
     }
 
     private void drop(int count) {
@@ -25,71 +29,70 @@ public class Day17 {
         shapeCount = 0;
         Field field = new Field();
         for (int i = 0; i < count; i++) {
-            List<Position> current = nextShape();
-            int ymax = field.towerHeight();
-            current = move(current, 2, ymax + 3);
+            Shape shape = nextShape();
+            long ymax = field.towerHeight();
+            int dx = 2;
+            long dy = ymax + 3;
             while (true) {
-                int dx = nextMove();
-                List<Position> afterMove = move(current, dx, 0);
-                if (inBounds(afterMove) && field.noCommon(afterMove)) {
-                    current = afterMove;
+                int movex = nextMove();
+                if (dx + movex >= 0 && dx + movex < shape.xlimit && field.noCommon(shape.positions, dx + movex, dy)) {
+                    dx += movex;
                 }
-                List<Position> afterFall = move(current, 0, -1);
-                if (inBounds(afterFall) && field.noCommon(afterFall)) {
-                    current = afterFall;
+                if (dy - 1 > 0 && field.noCommon(shape.positions, dx, dy - 1)) {
+                    dy--;
                 } else {
                     break;
                 }
             }
 //            printField(field, current);
-            field.addAll(current);
+            field.addAll(shape.positions, dx, dy);
         }
         System.out.println(field.towerHeight());
     }
 
-    private void printField(Field field, List<Position> current) {
-        int h = field.towerHeight() + 4;
-        for (int y = h; y >= 0;  y--) {
-            System.out.print('|');
-            for (int x = 0; x < 7; x++) {
-                Position position = new Position(x, y);
-                System.out.print(field.field.contains(position) ? '#' :
-                        (current.contains(position) ? '@' : '.'));
-            }
-            System.out.println('|');
-        }
-        System.out.println("+-------+");
-    }
+//    private void printField(Field field, List<Position> current) {
+//        long h = field.towerHeight() + 4;
+//        for (long y = h; y >= 0; y--) {
+//            System.out.print('|');
+//            for (int x = 0; x < 7; x++) {
+//                Position position = new Position(x, y);
+//                System.out.print(field.contains(position.x, position.y) ? '#' :
+//                        (current.contains(position) ? '@' : '.'));
+//            }
+//            System.out.println('|');
+//        }
+//        System.out.println("+-------+");
+//    }
 
     private void init() throws IOException {
         moves = Files.readString(new File("input17.txt").toPath()).trim();
-        shapes.add(List.of(
+        shapes.add(new Shape(
                 new Position(0, 0),
                 new Position(1, 0),
                 new Position(2, 0),
                 new Position(3, 0)
         ));
-        shapes.add(List.of(
+        shapes.add(new Shape(
                 new Position(1, 0),
                 new Position(0, 1),
                 new Position(1, 1),
                 new Position(2, 1),
                 new Position(1, 2)
         ));
-        shapes.add(List.of(
+        shapes.add(new Shape(
                 new Position(2, 2),
                 new Position(2, 1),
                 new Position(0, 0),
                 new Position(1, 0),
                 new Position(2, 0)
         ));
-        shapes.add(List.of(
+        shapes.add(new Shape(
                 new Position(0, 0),
                 new Position(0, 1),
                 new Position(0, 2),
                 new Position(0, 3)
         ));
-        shapes.add(List.of(
+        shapes.add(new Shape(
                 new Position(0, 0),
                 new Position(0, 1),
                 new Position(1, 0),
@@ -105,8 +108,8 @@ public class Day17 {
         return c == '<' ? -1 : 1;
     }
 
-    private List<Position> nextShape() {
-        List<Position> result = shapes.get(shapeCount++);
+    private Shape nextShape() {
+        Shape result = shapes.get(shapeCount++);
         if (shapeCount == shapes.size()) {
             shapeCount = 0;
         }
@@ -121,63 +124,86 @@ public class Day17 {
             this.x = x;
             this.y = y;
         }
-
-        public Position add(int dx, int dy) {
-            return new Position(x + dx, y + dy);
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Position position = (Position) o;
-            return x == position.x && y == position.y;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(x, y);
-        }
     }
 
-    private boolean inBounds(List<Position> positions) {
-        return positions.stream()
-                .allMatch(position -> position.x >= 0 && position.x < 7 && position.y >= 0);
-    }
+    static class RollArray {
+        static final int HPART = 100;
+        int start = 0;
+        long ystart = 0;
+        boolean[][] array = new boolean[HPART][7];
 
-    private List<Position> move(List<Position> positions, int dx, int dy) {
-        return positions.stream()
-                .map(position -> position.add(dx, dy))
-                .collect(Collectors.toList());
-    }
-
-    static class Field
-    {
-        int h = 0;
-        Set<Position> field = new HashSet<>();
-
-        public void addAll(List<Position> positions)
+        int getIndex(long y)
         {
-            for (Position position : positions) {
-                h = Math.max(h, position.y + 1);
-                field.add(position);
+            if (y < ystart || y >= ystart + HPART) {
+                throw new IllegalArgumentException();
             }
-            field = field.stream()
-                    .filter(p -> p.y >= h - 50)
-                    .collect(Collectors.toSet());
+            long r = y - ystart;
+            return ((int)r + start) % HPART;
         }
 
-        public boolean noCommon(List<Position> a) {
+        boolean contains(int x,long y)
+        {
+            if (y >= ystart + HPART) {
+                return false;
+            }
+            return array[getIndex(y)][x];
+        }
+
+        void add(int x,long y)
+        {
+            while (y >= ystart + HPART) {
+                Arrays.fill(array[start], false);
+                start = (start + 1) % HPART;
+                ystart++;
+            }
+            array[getIndex(y)][x] = true;
+        }
+    }
+
+    static class Field {
+        long h = 0;
+        RollArray rollArray = new RollArray();
+
+        public void addAll(Position[] positions, int dx, long dy) {
+            for (Position position : positions) {
+                h = Math.max(h, position.y + dy + 1);
+                add(position.x + dx, position.y + dy);
+            }
+        }
+
+        private void add(int px,long py) {
+            rollArray.add(px, py);
+        }
+
+        public boolean contains(int px, long py) {
+            return rollArray.contains(px, py);
+        }
+
+        public boolean noCommon(Position[] a, int dx, long dy) {
             for (Position position : a) {
-                if (field.contains(position)) {
+                if (contains(position.x + dx, position.y + dy)) {
                     return false;
                 }
             }
             return true;
         }
 
-        public int towerHeight() {
+        public long towerHeight() {
             return h;
+        }
+    }
+
+    static class Shape
+    {
+        Position[] positions;
+        int xlimit;
+
+        public Shape(Position... positions) {
+            this.positions = positions;
+            xlimit = 7 - Arrays.stream(positions)
+                    .mapToInt(p -> p.x)
+                    .max()
+                    .orElseThrow();
         }
     }
 }
