2ffc07d3e6b863289d7cb3f715b3029423d18b31
Johan de Jong <johan.de.jong@sogeti.com>
2022-12-19 09:19:43 +0100

    Day 19 part 1.

---
+++ src/Day19.java
@@ -0,0 +1,266 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Day19 {
+    List<Blueprint> blueprintList;
+
+    public static void main(String[] args) throws IOException {
+        new Day19().start();
+    }
+
+    private void start() throws IOException {
+        blueprintList = Files.readAllLines(new File("input19.txt").toPath()).stream()
+                .map(Blueprint::new)
+                .collect(Collectors.toList());
+        long first = 0;
+        for (int i = 0; i < blueprintList.size(); i++) {
+            Blueprint blueprint = blueprintList.get(i);
+            long quality = blueprint.first();
+            System.out.println(i + ": " + quality);
+            first += ((i + 1) * quality);
+        }
+        System.out.println("First: " + first);
+    }
+
+    static class Blueprint {
+        final Map<String, Amount> costs = new HashMap<>();
+
+        public Blueprint(String line) {
+            line = line.substring(line.indexOf(':') + 2);
+            String[] split = line.split("\\.");
+            for (String s : split) {
+                String[] sp = s.trim().split(" ");
+                String name = sp[1];
+                Amount cost = new Amount();
+                for (int i = 4; i < sp.length; i += 3) {
+                    int amount = Integer.parseInt(sp[i]);
+                    cost.set(sp[i + 1], amount);
+                }
+                costs.put(name, cost);
+            }
+            System.out.println();
+        }
+
+        public int first() {
+            Set<State> current = new HashSet<>();
+            current.add(new State());
+            for (int i = 0; i < 24; i++) {
+                Set<State> next = new HashSet<>();
+                for (State state : current) {
+                    next.addAll(state.getNextStates());
+                }
+                current = next;
+                if (i != 23) {
+//                    current = prune(current);
+                    current = pruneOnRobots(current);
+                    current = pruneOnCollected(current);
+                }
+                System.out.println(i + " -> " + current.size());
+            }
+            return current.stream().mapToInt(state -> state.collected.get("geode")).max().orElse(0);
+        }
+
+        private Set<State> prune(Set<State> states) {
+            Set<State> result = new HashSet<>();
+            for (State state : states) {
+                boolean betterState = states.stream()
+                        .anyMatch(otherState -> state != otherState &&
+                                state.collected.less(otherState.collected) &&
+                                state.robots.less(otherState.robots));
+                if (!betterState) {
+                    result.add(state);
+                }
+            }
+            return result;
+        }
+
+        private Set<State> pruneOnRobots(Set<State> next) {
+            Set<State> result = new HashSet<>();
+            Map<Amount, List<State>> map = next.stream().
+                    collect(Collectors.groupingBy(state -> state.robots));
+            for (var entry : map.entrySet()) {
+                result.addAll(pruneOnRobots(entry.getValue()));
+            }
+            return result;
+        }
+
+        private List<State> pruneOnRobots(List<State> states) {
+            List<State> result = new ArrayList<>();
+            for (State state : states) {
+                boolean betterState = states.stream()
+                        .anyMatch(otherState -> otherState != state && state.collected.less(otherState.collected));
+                if (!betterState) {
+                    result.add(state);
+                }
+            }
+            return result;
+        }
+
+        private Set<State> pruneOnCollected(Set<State> next) {
+            Set<State> result = new HashSet<>();
+            Map<Amount, List<State>> map = next.stream().
+                    collect(Collectors.groupingBy(state -> state.collected));
+            for (var entry : map.entrySet()) {
+                result.addAll(pruneOnCollected(entry.getValue()));
+            }
+            return result;
+        }
+
+        private List<State> pruneOnCollected(List<State> states) {
+            List<State> result = new ArrayList<>();
+            for (State state : states) {
+                boolean betterState = states.stream()
+                        .anyMatch(otherState -> otherState != state && state.robots.less(otherState.robots));
+                if (!betterState) {
+                    result.add(state);
+                }
+            }
+            return result;
+        }
+
+        class State {
+            final Amount collected;
+            final Amount robots;
+            final int didNotBuyMask;
+
+            State() {
+                collected = new Amount();
+                robots = new Amount();
+                robots.set("ore", 1);
+                didNotBuyMask = 0;
+            }
+
+            public State(Amount collected, Amount robots, int didNotBuyMask) {
+                this.collected = collected;
+                this.robots = robots;
+                this.didNotBuyMask = didNotBuyMask;
+            }
+
+            List<State> getNextStates() {
+                List<State> result = new ArrayList<>();
+                int canPayMask = didNotBuyMask;
+                for (Map.Entry<String, Amount> entry : costs.entrySet()) {
+                    int keyIndex = Amount.MAP.get(entry.getKey());
+                    int keyMask = 1 << keyIndex;
+                    if (collected.canPay(entry.getValue()) && (didNotBuyMask & keyMask) == 0) {
+                        canPayMask |= keyMask;
+                        Amount nextRobots = new Amount(robots);
+                        nextRobots.amounts[keyIndex]++;
+                        Amount collectedAfterPaying = new Amount(collected);
+                        collectedAfterPaying.pay(entry.getValue());
+
+                        Amount nextCollected = collectResources(collectedAfterPaying);
+
+                        result.add(new State(nextCollected, nextRobots, 0));
+                    }
+                }
+
+                Amount nextCollected = collectResources(collected);
+                result.add(new State(nextCollected, robots, canPayMask));
+
+                return result;
+            }
+
+            private Amount collectResources(Amount collected1) {
+                Amount result = new Amount(collected1);
+                result.add(robots);
+                return result;
+            }
+
+            @Override
+            public boolean equals(Object o) {
+                if (this == o) return true;
+                if (o == null || getClass() != o.getClass()) return false;
+                State state = (State) o;
+                return collected.equals(state.collected) && robots.equals(state.robots);
+            }
+
+            @Override
+            public int hashCode() {
+                return Objects.hash(collected, robots);
+            }
+
+            @Override
+            public String toString() {
+                return "State{" +
+                        "collected=" + collected +
+                        ", robots=" + robots +
+                        '}';
+            }
+        }
+    }
+
+    static class Amount {
+        static final Map<String, Integer> MAP = Map.of("ore", 0, "clay", 1, "obsidian", 2, "geode", 3);
+
+        final int[] amounts = new int[4];
+
+        public Amount() {
+        }
+
+        public Amount(Amount amount) {
+            System.arraycopy(amount.amounts, 0, amounts, 0, amounts.length);
+        }
+
+        public int get(String name) {
+            return amounts[MAP.get(name)];
+        }
+
+        public void set(String name, int value) {
+            amounts[MAP.get(name)] = value;
+        }
+
+        public boolean canPay(Amount costs) {
+            for (int i = 0; i < 4; i++) {
+                if (amounts[i] < costs.amounts[i]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public void pay(Amount costs) {
+            for (int i = 0; i < 4; i++) {
+                amounts[i] -= costs.amounts[i];
+            }
+        }
+
+        public void add(Amount costs) {
+            for (int i = 0; i < 4; i++) {
+                amounts[i] += costs.amounts[i];
+            }
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Amount amount = (Amount) o;
+            return Arrays.equals(amounts, amount.amounts);
+        }
+
+        @Override
+        public int hashCode() {
+            return Arrays.hashCode(amounts);
+        }
+
+        public boolean less(Amount o) {
+            for (int i = 0; i < 4; i++) {
+                if (amounts[i] >= o.amounts[i]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        @Override
+        public String toString() {
+            return MAP.entrySet().stream()
+                    .map(entry -> entry.getKey() + "=" + amounts[entry.getValue()])
+                    .collect(Collectors.joining(","));
+        }
+    }
+}
